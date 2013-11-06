0ac138ae776711d130450dff010c2620bc5161f0
Sebastian Freundt <freundt@ga-group.nl>
2013-11-06 10:06:45 +0000

    fix, turn abs mmys into ocos too
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/mmy.h
+++ src/mmy.h
@@ -148,13 +148,16 @@ truf_mmy_oco(truf_mmy_t ym, unsigned int year)
 {
 /* return a trym relative to YEAR, i.e. F0 for F2000 for year == 2000
  * or leave as is if YM is relative already. */
-	register signed int yr = truf_mmy_year(ym);
+	register signed int y = truf_mmy_year(ym);
+	register unsigned int m = truf_mmy_mon(ym);
+	register unsigned int d = truf_mmy_day(ym);
 	if (!truf_mmy_abs_p(ym)) {
-		register signed int m = truf_mmy_mon(ym);
-		register signed int d = truf_mmy_day(ym);
-		return make_truf_mmy(yr + year, m, d ?: -1U);
+		y += year;
 	}
-	return ym;
+	if (!d) {
+		d = -1U;
+	}
+	return make_truf_mmy(y + year, m, d ?: -1U);
 }
 
 static inline __attribute__((pure, const)) bool
