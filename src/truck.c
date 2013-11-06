a334b8dce8be1e2bf7790247d3b0da561c444a9d
Sebastian Freundt <freundt@ga-group.nl>
2013-11-06 11:57:18 +0000

    minor, provide position sub-command
    position is what Rudi needs, given a date or time and a file with trod directives
    output a list of invested contracts in echs format.
    
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/truck.c
+++ src/truck.c
@@ -290,6 +290,30 @@ static const struct co_pop_res_s {
 	return 0;
 }
 
+declcoru(co_inst_rdr, {
+		const char *const *dt;
+		size_t ndt;
+	}, {});
+
+static const struct co_rdr_res_s*
+defcoru(co_inst_rdr, c, UNUSED(arg))
+{
+/* coroutine for the reader of the tseries */
+	const char *const *dt = c->dt;
+	const char *const *const edt = c->dt + c->ndt;
+	/* we'll yield a rdr_res */
+	struct co_rdr_res_s res;
+
+	for (; dt < edt; dt++) {
+		char *on;
+		res.t = dt_strp(*dt, &on);
+		res.ln = *dt;
+		res.lz = on - *dt;
+		yield(res);
+	}
+	return 0;
+}
+
 
 /* auxils between coru and beef routines */
 static void
@@ -461,6 +485,62 @@ truf_filt_tser_file(struct truf_ctx_s ctx[static 1], const char *tser)
 	free_coru(pop);
 	fini_coru();
 	fclose(f);
+	return 0;
+}
+
+static int
+truf_prnt_poss(struct truf_ctx_s ctx[static 1U], char *const dt[], size_t ndt)
+{
+	coru_t rdr;
+	coru_t pop;
+
+	init_coru();
+	if (ndt == 0U) {
+		/* read date/times from stdin */
+		rdr = make_coru(co_echs_rdr, stdin);
+	} else {
+		rdr = make_coru(co_inst_rdr, dt, ndt);
+	}
+	pop = make_coru(co_echs_pop, ctx->q);
+
+	const struct co_pop_res_s *ev;
+	const struct co_rdr_res_s *ln;
+	for (ln = next(rdr), ev = next(pop); ln != NULL;) {
+		/* in between date/times from the RDR find trods */
+		for (;
+		     LIKELY(ev != NULL) &&
+			     UNLIKELY(!echs_instant_lt_p(ln->t, ev->t));
+		     ev = next(pop)) {
+			truf_mmy_t c = ev->edge.sym;
+
+			if (ev->edge.exp == 0.df) {
+				lstk_kick((truf_trod_t){c, ev->edge.exp});
+			} else {
+				lstk_join((truf_trod_t){c, ev->edge.exp});
+			}
+		}
+
+		do {
+			char buf[1024U];
+			char *bp = buf;
+			const char *const ep = buf + sizeof(buf);
+
+			bp += dt_strf(bp, ep - bp, ln->t);
+			*bp++ = '\t';
+			for (size_t i = imin; i < imax; i++) {
+				if (lstk[i].d.sym) {
+					truf_trod_wr(bp, ep - bp, lstk[i].d);
+					puts(buf);
+				}
+			}
+		} while (LIKELY((ln = next(rdr)) != NULL) &&
+			 (UNLIKELY(ev == NULL) ||
+			  LIKELY(echs_instant_lt_p(ln->t, ev->t))));
+	}
+
+	free_coru(rdr);
+	free_coru(pop);
+	fini_coru();
 	return 0;
 }
 
@@ -744,6 +824,42 @@ out:
 	return res;
 }
 
+static int
+cmd_position(struct truf_args_info argi[static 1U])
+{
+	static const char usg[] = "\
+Usage: truffle position TROD-FILE [DATE/TIME]...\n";
+	static struct truf_ctx_s ctx[1];
+	int res = 0;
+
+	if (argi->inputs_num < 2U) {
+		fputs(usg, stderr);
+		res = 1;
+		goto out;
+	} else if (UNLIKELY((ctx->q = make_truf_wheap()) == NULL)) {
+		res = 1;
+		goto out;
+	}
+
+	with (const char *fn = argi->inputs[1U]) {
+		if (UNLIKELY(truf_read_trod_file(ctx, fn) < 0)) {
+			error("cannot open trod file `%s'", fn);
+			res = 1;
+			goto out;
+		}
+	}
+
+	/* just print them now */
+	truf_prnt_poss(ctx, argi->inputs + 2U, argi->inputs_num - 2U);
+
+out:
+	if (LIKELY(ctx->q != NULL)) {
+		free_truf_wheap(ctx->q);
+	}
+	truf_free_trods();
+	return res;
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -768,6 +884,8 @@ main(int argc, char *argv[])
 			res = cmd_migrate(argi);
 		} else if (!strcmp(cmd, "filter")) {
 			res = cmd_filter(argi);
+		} else if (!strcmp(cmd, "position")) {
+			res = cmd_position(argi);
 		} else {
 		nocmd:
 			error("No valid command specified.\n\
