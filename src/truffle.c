b6869990801e0a4a1526d2bb1680fc90bc72c022
Sebastian Freundt <freundt@ga-group.nl>
2013-11-20 07:08:51 +0000

    hygiene, suffix all float literals
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/truffle.c
+++ src/truffle.c
@@ -688,16 +688,16 @@ make_trod_from_cline(const struct cline_s *p, daisy_t when)
 
 		if (when == l2) {
 			/* something happened at l2 */
-			if (n2->y == 0.0 && n1->y != 0.0) {
+			if (n2->y == 0.d && n1->y != 0.d) {
 				res.exp = 0.df;
-			} else if (n2->y != 0.0 && n1->y == 0.0) {
+			} else if (n2->y != 0.d && n1->y == 0.d) {
 				res.exp = 1.df;
 			} else {
 				continue;
 			}
 		} else if (j == 0 && when == l1) {
 			/* something happened at l1 */
-			if (UNLIKELY(n1->y != 0.0)) {
+			if (UNLIKELY(n1->y != 0.d)) {
 				res.exp = 1.df;
 			} else {
 				continue;
